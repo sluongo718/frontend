92f92f45f70178e8df2a8f510acc806b5f815610
sluongo718 <sluongo718@yahoo.com>
2021-10-12 18:19:35 -0400

    setting up review routes

---
+++ src/components/reviewForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+
+class ReviewForm extends Component {
+    render(){
+        return <div>
+            
+        </div>
+    }
+}
+
+export default ReviewForm
