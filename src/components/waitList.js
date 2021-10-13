74643a1428084491dfc8c5e9ae37c0b555985325
sluongo718 <sluongo718@yahoo.com>
2021-10-13 03:44:45 -0400

    started waitlist controller/route

---
+++ src/components/waitList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import {connect} from 'react-redux'
+
+class WaitList extends Component {
+
+    render(){
+        return <div>
+            heellllooooooo
+        </div>
+    }
+
+}
+
+export default WaitList
--- src/containers/waitListContainer.js
+++ src/containers/waitListContainer.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import waitList from '../components/waitList'
 
 
 class WaitListContainer extends Component {
     render(){
         return <div>
                 <Switch>
-                  
+                  <Route exact path="/waitlist"  component={waitList} />
                 </Switch>
         </div>
     }
