d84a61cb34d0cb4aa9c3e3448463e06fc8d89a58
sluongo718 <sluongo718@yahoo.com>
2021-10-14 19:51:17 -0400

    can delete waitlist

--- src/components/waitList.js
+++ src/components/waitList.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import {connect} from 'react-redux'
+import {deleteParty} from '../actions/menuItemActions'
 
 class WaitList extends Component {
 
 
+    handleclick = (party) => {
+        this.props.deleteParty(party.id)
+    }
 
     render(){
         return <div>
            {this.props.waitList.map((party) =>(
                <li key={party.id}>
-                   {party.name}
+                   {party.name} <button id={party.id} onClick={() => this.handleclick(party) } >Delete Party</button>
                </li>
            ))}
         </div>
@@ -17,4 +21,4 @@ class WaitList extends Component {
 
 }
 
-export default WaitList
+export default connect(null, {deleteParty})(WaitList) 
